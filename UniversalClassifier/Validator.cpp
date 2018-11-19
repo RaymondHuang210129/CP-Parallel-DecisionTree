f7463291d067f28ff95d141033e50af49bf911fe
則叡 黃 <raymond860909@gmail.com>
2018-11-20 00:40:27 +0800

    Change to linux compatible encoding

--- UniversalClassifier/Validator.cpp
+++ UniversalClassifier/Validator.cpp
@@ -3,47 +3,50 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
-#include <random>
 
-//Create random forest
-vector<Node*> RandomForestCreater(vector<vector<double>> dataset)
+using namespace std;
+
+Validator::Validator()
 {
-	random_device rd;
-	default_random_engine gen = default_random_engine(rd());
-	
-	
-	
-	//State the number to be selected for each tree
-	int numSelectFeature = sqrt(dataset[0].size() - 1) + 1;
-	//State the number of tree to construct
-	int numTree = 64;
 
-	vector<Node*> trees;
-	trees.resize(numTree);
+}
 
-	//randomly select features (non-duplicate)
-	vector<int> features(dataset[0].size() - 1);
-	for (int i = 0; i < dataset[0].size() - 1; i++) features[i] = i;
 
+vector<vector<double>> Validator::getResult(vector<Node*> trees, vector<vector<double>> dataset)
+{
+	//result: 0->prediction 1->fact
+	vector<vector<double>> result;
+	result.resize(dataset.size());
+
+	//vote data structure, initalize to 0 for all classes
+	
 #pragma omp parallel for
-
-	for (int i = 0; i < numTree; i++)
+	for (int i = 0; i < dataset.size(); i++)
 	{
-		//generate a list of selected features
-		vector<int> features2 = features;
-		shuffle(features2.begin(), features2.end(), gen);
-		vector<int> selectedFeature(features2.begin(), features2.begin() + numSelectFeature);
-		
-		//randomly select samples (with duplicate)
-		uniform_int_distribution<int> dis(0, dataset.size() - 1);
-		vector<vector<double>> selectedSample;
-		for (int j = 0; j < dataset.size(); j++)
+		vector<int> vote(dataset.size() - 1, 0);
+		//Iterate with different trees
+		for (int j = 0; j < trees.size(); j++)
 		{
-			selectedSample.push_back(dataset[dis(gen)]);
+			//Start from the root of tree
+			Node* currentLocation = trees[j];
+			while (1)
+			{
+				if (currentLocation->isLeaf)//Reach the leaf
+				{
+					//vote
+					vote[currentLocation->leafClass]++;
+					break;
+				}
+				if (dataset[i][currentLocation->selectedFeature] > currentLocation->gapValue)
+					currentLocation = currentLocation->higherNode;
+				else
+					currentLocation = currentLocation->lowerNode;
+			}
 		}
-		cout << "-";
-		trees[i] = ConstructTree(dataset, 0, "entropy", selectedFeature);
-	}
 
-	return trees;
+		//find the class with maximum vote and put into result
+		result[i] = { (double)distance(vote.begin(), max_element(vote.begin(), vote.end())), dataset[i][dataset[0].size() - 1] };
+
+	}
+	return result;
 }
