6c3ddc0ab14d52abaa4ea5eff85767866ec39379
samuelyen <s930555samuel@gmail.com>
2018-12-09 11:59:07 +0800

    finish the pthread version

--- UniversalClassifier/RandomForestCreater.cpp
+++ UniversalClassifier/RandomForestCreater.cpp
@@ -4,21 +4,51 @@
 #include <iostream>
 #include <algorithm>
 #include <random>
+#include <semaphore.h>
+#include <pthread.h>
+#define MAX_CORE 4
+
+pthread_t thread_id[MAX_CORE];
+
+class parameter_for_function{
+public:
+	vector<int> features;
+	int numSelectFeature;
+	vector<vector<double>> dataset;
+	int id;
+	int numTree;
+	default_random_engine gen;
+
+	parameter_for_function(vector<int> in_feature , int in_numSelectedFeature , vector<vector<double>> in_dataset,int in_id , int in_numTree,default_random_engine in_gen){
+		features=in_feature;
+		numSelectFeature = in_numSelectedFeature;
+		dataset = in_dataset;
+		id = in_id;
+		numTree = in_numTree;
+		gen=in_gen;
+	}
+};
+
+sem_t mutex_for_node_vector;
+vector<Node*> trees;
+void *for_loop_creation(void *);
+
 
 //Create random forest
 vector<Node*> RandomForestCreater(vector<vector<double>> dataset)
 {
 	random_device rd;
 	default_random_engine gen = default_random_engine(rd());
+	void *ret;
 	
-	
+	sem_init(&mutex_for_node_vector,0,1);
 	
 	//State the number to be selected for each tree
 	int numSelectFeature = sqrt(dataset[0].size() - 1) + 1;
 	//State the number of tree to construct
 	int numTree = 64;
 
-	vector<Node*> trees;
+	
 	trees.resize(numTree);
 
 	//randomly select features (non-duplicate)
@@ -26,8 +56,13 @@ vector<Node*> RandomForestCreater(vector<vector<double>> dataset)
 	for (int i = 0; i < dataset[0].size() - 1; i++) features[i] = i;
 
 //#pragma omp parallel for
+		parameter_for_function *input_for_function_ptr[4];
+		for(int k=0;k<MAX_CORE;k++){
+		input_for_function_ptr[k]= new parameter_for_function(features,numSelectFeature,dataset,k,numTree,gen);
+		pthread_create(&thread_id[k], NULL, &for_loop_creation , input_for_function_ptr[k]);	
+		}
 
-	for (int i = 0; i < numTree; i++)
+	/*for (int i = 0; i < numTree; i++)	//input:feature,numselect,feature,dataset
 	{
 		//generate a list of selected features
 		vector<int> features2 = features;
@@ -43,7 +78,34 @@ vector<Node*> RandomForestCreater(vector<vector<double>> dataset)
 		}
 		cout << "-" << flush;
 		trees[i] = ConstructTree(dataset, 0, "entropy", selectedFeature);
-	}
-
+	}*/
+		for(int k=0;k<MAX_CORE;k++){
+			pthread_join(thread_id[k],&ret);
+			delete input_for_function_ptr[k];
+		}
+		
 	return trees;
+}
+
+
+void *for_loop_creation(void *input_for_function_param){
+	parameter_for_function * input_for_function=(parameter_for_function *)input_for_function_param;
+	for (int i = ceil(input_for_function->numTree/MAX_CORE)*(input_for_function->id); i < ceil(input_for_function->numTree/MAX_CORE)*(input_for_function->id+1); i++)	//input:feature,numselect,feature,dataset
+	{
+		//generate a list of selected features
+		vector<int> features2 = input_for_function->features;
+		shuffle(features2.begin(), features2.end(), input_for_function->gen);
+		vector<int> selectedFeature(features2.begin(), features2.begin() + input_for_function->numSelectFeature);
+		
+		//randomly select samples (with duplicate)
+		uniform_int_distribution<int> dis(0, input_for_function->dataset.size() - 1);
+		vector<vector<double>> selectedSample;
+		for (int j = 0; j < input_for_function->dataset.size(); j++)
+		{
+			selectedSample.push_back(input_for_function->dataset[dis(input_for_function->gen)]);
+		}
+		cout << "-" << flush;
+		trees[i] = ConstructTree(input_for_function->dataset, 0, "entropy", selectedFeature);
+	}
+	return NULL;
 }
